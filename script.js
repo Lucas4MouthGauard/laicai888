413a8b2a96828d72537c5f56df9d8cfd3c050e53
Lucas4MouthGauard <biaoli888@outlook.com>
2025-07-04 18:48:09 +0800

    更新CA地址和BUY链接

--- script.js
+++ script.js
@@ -381,7 +381,7 @@ function toggleMobileMenu() {
 
 // 复制CA地址
 function copyCA() {
-    const caText = '000000000000000000000000000';
+    const caText = 'FRNGPMnGZbPCVM6ZwubYc4RzCJbxK5B3wfbrQYRibonk';
     
     if (navigator.clipboard) {
         navigator.clipboard.writeText(caText).then(() => {
@@ -407,8 +407,8 @@ function fallbackCopy(text) {
 
 // 购买代币
 function buyToken() {
-    // 暂时无功能，不显示任何提示
-    return;
+    // 跳转到购买页面
+    window.open('https://letsbonk.fun/token/FRNGPMnGZbPCVM6ZwubYc4RzCJbxK5B3wfbrQYRibonk', '_blank');
 }
 
 // 音乐控制功能
